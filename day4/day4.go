b6dabb160ba65de736b562a9a5824808a4c8d884
Marius Niemenmaa <marius.niemenmaa@outlook.com>
2023-12-06 20:23:22 +0200

    Refactor day4 into single file

--- day4/day4.go
+++ day4/day4.go
@@ -10,6 +10,11 @@ import (
 	"strings"
 )
 
+type Game struct {
+	winningNums  []int
+	selectedNums []int
+}
+
 func main() {
 	file, err := os.Open("./example_input")
 	if err != nil {
@@ -25,15 +30,50 @@ func main() {
 		games = append(games, parseGame(line))
 	}
 
+	part1(games)
+	part2(games)
+}
+
+func part1(games []Game) {
 	total := 0
 	for _, game := range games {
 		total += gameToPoints(game)
 	}
 
-	fmt.Println(total)
+	fmt.Println("Part 1: ", total)
+}
+
+func part2(games []Game) {
+	cards := make([]int, len(games))
+	for i := 0; i < len(games); i++ {
+		cards = append(cards, 0)
+	}
+
+	total := 0
+	for i, game := range games {
+		cards[i]++
+		matches := gameToMatches(game)
+		for j := 1; j <= matches; j++ {
+			cards[i+j] += 1 * cards[i]
+		}
+
+		total += cards[i]
+	}
+
+	fmt.Println("Part 2: ", total)
 }
 
 func gameToPoints(game Game) int {
+	matches := gameToMatches(game)
+
+	if matches < 1 {
+		return 0
+	}
+
+	return int(math.Pow(2.0, float64(matches-1)))
+}
+
+func gameToMatches(game Game) int {
 	matches := 0
 	for _, selectedNum := range game.selectedNums {
 		match := false
@@ -49,11 +89,7 @@ func gameToPoints(game Game) int {
 		}
 	}
 
-    if matches < 1 {
-        return 0
-    }
-
-	return int(math.Pow(2.0, float64(matches-1)))
+	return matches
 }
 
 func parseGame(row string) Game {
@@ -64,9 +100,9 @@ func parseGame(row string) Game {
 	winningNumsStr := strings.Split(numsSplit[0], " ")
 	for _, numStr := range winningNumsStr {
 		num, err := strconv.Atoi(numStr)
-        if err != nil {
-            continue
-        }
+		if err != nil {
+			continue
+		}
 		winningNums = append(winningNums, num)
 	}
 
@@ -74,19 +110,14 @@ func parseGame(row string) Game {
 	selectedNumsStr := strings.Split(numsSplit[len(numsSplit)-1], " ")
 	for _, numStr := range selectedNumsStr {
 		num, err := strconv.Atoi(numStr)
-        if err != nil {
-            continue
-        }
+		if err != nil {
+			continue
+		}
 		selectedNums = append(selectedNums, num)
 	}
 
 	return Game{
-        winningNums: winningNums,
-        selectedNums: selectedNums,
-    }
-}
-
-type Game struct {
-	winningNums  []int
-	selectedNums []int
+		winningNums:  winningNums,
+		selectedNums: selectedNums,
+	}
 }
